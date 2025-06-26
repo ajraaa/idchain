920acfd6d59df794ba88a7c90c4078c161532c7c
ajraaa <azr.tech00@gmail.com>
2025-06-26 21:31:16 +0700

    added some feature and some refinement to the code: 1. mapping for daftar pemohonan 2. event for telling the frontend after kalurahan and dukcapil has verified the request 3. helper for the frontend to map the status and jenis permohonan enum 4. function for the frontend to get the request id according to applicant address 5. function for the frontend to get all the request according to id got from the function before

--- contracts/PencatatanSipil.sol
+++ contracts/PencatatanSipil.sol
@@ -22,7 +22,7 @@ contract PencatatanSipil is KontrolAkses {
         DisetujuiDukcapil,
         DitolakDukcapil
     }
-    
+
     struct Permohonan {
         uint256 id; // ID permohonan
         address pemohon; // alamat pemohon
@@ -39,7 +39,8 @@ contract PencatatanSipil is KontrolAkses {
     }
 
     uint256 public jumlahPermohonan;
-    mapping (uint256 => Permohonan) permohonans;
+    mapping(uint256 => Permohonan) permohonans;
+    mapping(address => uint256[]) public daftarPermohonanPemohon;
 
     event PermohonanDiajukan(
         uint256 indexed id,
@@ -49,7 +50,7 @@ contract PencatatanSipil is KontrolAkses {
         uint256 waktu
     );
 
-    event VerifikasiKalurahan (
+    event VerifikasiKalurahan(
         uint256 indexed id,
         address indexed verifikator,
         bool disetujui,
@@ -57,21 +58,55 @@ contract PencatatanSipil is KontrolAkses {
         uint256 waktu
     );
 
-    event VerifikasiDukcapil (
+    event VerifikasiDukcapil(
         uint256 indexed id,
         address indexed verifikator,
         bool disetujui,
         string alasan,
-        uint256 waktu 
+        uint256 waktu
     );
 
-    function SubmitPermohonan(JenisPermohonan _jenis, string calldata _cidIPFS)  external {
+    function getStatusPermohonan(
+        uint256 _id
+    ) external view returns (string memory) {
+        Status s = permohonans[_id].status;
+
+        if (s == Status.Diajukan) return "Diajukan";
+        if (s == Status.DiprosesKalurahan) return "Diproses Kalurahan";
+        if (s == Status.DisetujuiKalurahan) return "Disetujui Kalurahan";
+        if (s == Status.DitolakKalurahan) return "Ditolak Kalurahan";
+        if (s == Status.DiprosesDukcapil) return "Diproses Dukcapil";
+        if (s == Status.DisetujuiDukcapil) return "Disetujui Dukcapil";
+        if (s == Status.DitolakDukcapil) return "Ditolak Dukcapil";
+
+        return "Status Tidak Dikenal";
+    }
+
+    function getJenisPermohonan(
+        uint256 _id
+    ) external view returns (string memory) {
+        JenisPermohonan j = permohonans[_id].jenis;
+
+        if (j == JenisPermohonan.Kelahiran) return "Kelahiran";
+        if (j == JenisPermohonan.Kematian) return "Kematian";
+        if (j == JenisPermohonan.Kawin) return "Kawin";
+        if (j == JenisPermohonan.Cerai) return "Cerai";
+        if (j == JenisPermohonan.Pindah) return "Pindah";
+        if (j == JenisPermohonan.Datang) return "Datang";
+
+        return "Jenis Tidak Dikenal";
+    }
+
+    function SubmitPermohonan(
+        JenisPermohonan _jenis,
+        string calldata _cidIPFS
+    ) external {
         require(bytes(_cidIPFS).length > 0, "CID IPFS tidak boleh kosong.");
 
         uint256 idBaru = jumlahPermohonan++;
         jumlahPermohonan++;
 
-        permohonans[idBaru] = Permohonan ({
+        permohonans[idBaru] = Permohonan({
             id: idBaru,
             pemohon: msg.sender,
             jenis: _jenis,
@@ -86,13 +121,28 @@ contract PencatatanSipil is KontrolAkses {
             waktuVerifikasiDukcapil: 0
         });
 
-        emit PermohonanDiajukan(idBaru, msg.sender, _jenis, _cidIPFS, block.timestamp);
+        daftarPermohonanPemohon[msg.sender].push(idBaru);
+
+        emit PermohonanDiajukan(
+            idBaru,
+            msg.sender,
+            _jenis,
+            _cidIPFS,
+            block.timestamp
+        );
     }
 
-    function verifikasiKalurahan(uint256 _id, bool _disetujui, string calldata _alasan) external onlyKalurahan {
+    function verifikasiKalurahan(
+        uint256 _id,
+        bool _disetujui,
+        string calldata _alasan
+    ) external onlyKalurahan {
         Permohonan storage p = permohonans[_id];
 
-        require(p.status == Status.Diajukan, "Permohonan bukan dalam status Diajukan.");
+        require(
+            p.status == Status.Diajukan,
+            "Permohonan bukan dalam status Diajukan."
+        );
 
         if (_disetujui) {
             p.status = Status.DisetujuiKalurahan;
@@ -104,13 +154,26 @@ contract PencatatanSipil is KontrolAkses {
         p.verifikatorKalurahan = msg.sender;
         p.waktuVerifikasiKalurahan = block.timestamp;
 
-        emit VerifikasiKalurahan(_id, msg.sender, _disetujui, _alasan, block.timestamp);
+        emit VerifikasiKalurahan(
+            _id,
+            msg.sender,
+            _disetujui,
+            _alasan,
+            block.timestamp
+        );
     }
 
-    function verifikasiDukcapil(uint256 _id, bool _disetujui, string calldata _alasan) external onlyDukcapil {
+    function verifikasiDukcapil(
+        uint256 _id,
+        bool _disetujui,
+        string calldata _alasan
+    ) external onlyDukcapil {
         Permohonan storage p = permohonans[_id];
 
-        require(p.status == Status.DisetujuiKalurahan, "Permohonan belum disetujui oleh Kalurahan.");
+        require(
+            p.status == Status.DisetujuiKalurahan,
+            "Permohonan belum disetujui oleh Kalurahan."
+        );
 
         if (_disetujui) {
             p.status = Status.DisetujuiDukcapil;
@@ -122,7 +185,55 @@ contract PencatatanSipil is KontrolAkses {
         p.verifikatorDukcapil = msg.sender;
         p.waktuVerifikasiDukcapil = block.timestamp;
 
-        emit VerifikasiDukcapil(_id, msg.sender, _disetujui, _alasan, block.timestamp);
+        emit VerifikasiDukcapil(
+            _id,
+            msg.sender,
+            _disetujui,
+            _alasan,
+            block.timestamp
+        );
     }
 
+    function getPermohonanIDsByPemohon(
+        address _pemohon
+    ) external view returns (uint256[] memory) {
+        return daftarPermohonanPemohon[_pemohon];
+    }
+
+    function getPermohonan(
+        uint256 _id
+    )
+        external
+        view
+        returns (
+            uint256 id,
+            address pemohon,
+            JenisPermohonan jenis,
+            string memory cidIPFS,
+            Status status,
+            uint256 waktuPengajuan,
+            string memory alasanPenolakanKalurahan,
+            string memory alasanPenolakanDukcapil,
+            address verifikatorKalurahan,
+            uint256 waktuVerifikasiKalurahan,
+            address verifikatorDukcapil,
+            uint256 waktuVerifikasiDukcapil
+        )
+    {
+        Permohonan memory p = permohonans[_id];
+        return (
+            p.id,
+            p.pemohon,
+            p.jenis,
+            p.cidIPFS,
+            p.status,
+            p.waktuPengajuan,
+            p.alasanPenolakanKalurahan,
+            p.alasanPenolakanDukcapil,
+            p.verifikatorKalurahan,
+            p.waktuVerifikasiKalurahan,
+            p.verifikatorDukcapil,
+            p.waktuVerifikasiDukcapil
+        );
+    }
 }
