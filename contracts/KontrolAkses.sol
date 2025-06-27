94f17a5415f4fd677f129aefe0f62a360e5df1af
ajraaa <azr.tech00@gmail.com>
2025-06-27 18:04:56 +0700

    added function to remove authenicated kalurahan and dukcapil account

--- contracts/KontrolAkses.sol
+++ contracts/KontrolAkses.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.28;
 
 contract KontrolAkses {
     address public owner;
-    mapping (address => bool) public kalurahan;
-    mapping (address => bool) public dukcapil;
+    mapping(address => bool) public kalurahan;
+    mapping(address => bool) public dukcapil;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Hanya owner yang dapat melakukan ini.");
@@ -12,12 +12,18 @@ contract KontrolAkses {
     }
 
     modifier onlyKalurahan() {
-        require(kalurahan[msg.sender], "Hanya petugas kalurahan yang diizinkan melakukan ini.");
+        require(
+            kalurahan[msg.sender],
+            "Hanya petugas kalurahan yang diizinkan melakukan ini."
+        );
         _;
     }
 
     modifier onlyDukcapil() {
-        require(dukcapil[msg.sender], "Hanya petugas dukcapil yang diizinkan melakukan ini.");
+        require(
+            dukcapil[msg.sender],
+            "Hanya petugas dukcapil yang diizinkan melakukan ini."
+        );
         _;
     }
 
@@ -31,5 +37,13 @@ contract KontrolAkses {
 
     function tambahDukcapil(address _akun) external onlyOwner {
         dukcapil[_akun] = true;
+    }
+
+    function hapusKalurahan(address _akun) external onlyOwner {
+        kalurahan[_akun] = false;
+    }
+
+    function hapusDukcapil(address _akun) external onlyOwner {
+        dukcapil[_akun] = false;
     }
 }
